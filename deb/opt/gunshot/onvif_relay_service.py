4a043484fa92a954843c3093e4aa644e1d738be8
Anson Mansfield <amansfield@mantaro.com>
2021-02-11 12:19:34 -0500

    stub

--- deb/opt/gunshot/onvif_relay_service.py
+++ deb/opt/gunshot/onvif_relay_service.py
@@ -22,6 +22,7 @@ def get_relay_token(devmgmt):
     if config.onvif_relay.relay_type == "token":
         return config.onvif_relay.relay_id
     elif config.onvif_relay.relay_type == "number":
+        raise NotImplemented
         # TODO look up the relay token based on the index number
     else:
         raise NotImplemented
