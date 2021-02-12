3a62e1f02410ae44a14545f2c0038563a064c697
Anson Mansfield <amansfield@mantaro.com>
2021-02-12 15:04:21 -0500

    detector same as the others

--- deb/opt/gunshot/detector_service.py
+++ deb/opt/gunshot/detector_service.py
@@ -9,52 +9,34 @@ import settings
 config = settings.load('mqtt', 'detector', log=log.getChild('config'))
 
 import paho.mqtt.client as mqtt
-import socket
-import socketserver
+from net_conn import make_connection
 
-class GSDHandler(socketserver.BaseRequestHandler):
-    def setup(self, *args, **kwargs):
-        super().setup(*args, **kwargs)
+def on_connect(client, userdata, flags, rc):
+    mqlog.info("connected to broker")
+    client.subscribe(config.mqtt.topics.cmd_raw)
 
-        gsdlog.info("configuring socket")
-        self.request.setblocking(False)
+def on_message(client, userdata, msg):
+    mqlog.info("recieved message {}".format(repr(msg)))
+    gsdlog.info("sending event {}".format(repr(msg.payload)))
+    sock.sendall(msg.payload)
 
-        mqlog.info("setting up connection")
+log.info('setting up socket')
+with make_connection(config.detector.bind, None) as sock:
 
-        def on_connect(client, userdata, flags, rc):
-            mqlog.info("connected to broker")
-            client.subscribe(config.mqtt.topics.cmd_raw)
-
-        def on_message(client, userdata, msg):
-            mqlog.info("recieved command {}".format(repr(msg)))
-            gsdlog.info("sending command {}".format(repr(msg.payload)))
-            self.request.sendall(msg.payload)
-            # mqttlog.getChild(msg.topic.replace('/','.')).info(str(msg.payload))
-        
-        self.client = mqtt.Client()
-        self.client.on_connect = on_connect
-        self.client.on_message = on_message
-        self.client.connect(**config.mqtt.server)
-
-        log.info("ready")
-
-    def handle(self):
-        while True:
-            self.client.loop()
-            try:
-                data = self.request.recv(1024)
-                gsdlog.info("recieved event {}".format(repr(data)))
-                event = self.client.publish(config.mqtt.topics.evt_raw, data)
-                mqlog.info("published {}".format(repr(event)))
-            except BlockingIOError:
-                pass
-
-    def finish(self, *args, **kwargs):
-        super().finish(*args, **kwargs)
-        log.warning("disconnected")
-        self.client.disconnect()
-
-log.info("listening on {}".format(config.detector.listen))
-server = socketserver.TCPServer(socket.getaddrinfo(**config.detector.listen)[0][4], GSDHandler)
-log.info("waiting for connection")
-server.serve_forever()
+    mqlog.info('connnecting to MQTT')
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(**config.mqtt.server)
+    
+    while True:
+        client.loop()
+        try:
+            data = sock.recv(1024)
+        except BlockingIOError:
+            continue
+        if len(data) == 0:
+            raise Exception("recieved empty packet")
+        gsdlog.info("recieved command {}".format(repr(data)))
+        event = client.publish(config.mqtt.topics.evt_raw, data)
+        mqlog.info("published {}".format(repr(event)))
