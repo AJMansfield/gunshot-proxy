9a0dc5926c620519404fc82277c1e77c56e45c8d
Anson Mansfield <amansfield@mantaro.com>
2021-02-18 12:38:19 -0500

    move things around

--- deb/var/www/html/password.php
+++ deb/var/www/html/password.php
@@ -41,13 +41,15 @@ ini_set('display_errors',1); error_reporting(E_ALL);
 
 <div class="container">
 <h1 class="title">User Management</h1>
-<div>
-  <a href="password.php" class="btn btn-primary">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-    </svg>
-    Back to Main Page
-  </a>
+<div class="card">
+  <div class="card-body">
+    <a href="password.php" class="btn btn-primary">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+      </svg>
+      Back to Main Page
+    </a>
+  </div>
 </div>
 <div class="card">
   <div class="card-header h4">Change Password</div>
@@ -86,7 +88,7 @@ ini_set('display_errors',1); error_reporting(E_ALL);
         </div>
       </div>
       
-      <div class="form-group row">
+      <div>
         <button type="submit" class="btn btn-primary">Change Password</button>
       </div>
     </form>
