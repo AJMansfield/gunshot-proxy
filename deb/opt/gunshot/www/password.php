d418857996afca754aadabd45c26ac3c98ed77aa
Anson Mansfield <amansfield@mantaro.com>
2021-02-19 10:02:21 -0500

    migrate to nginx

--- deb/opt/gunshot/www/password.php
+++ deb/opt/gunshot/www/password.php
@@ -45,7 +45,7 @@ ini_set('display_errors',1); error_reporting(E_ALL);
 <h1 class="title">User Management</h1>
 <div class="card">
   <div class="card-body">
-    <a href="index.php" class="btn btn-primary">
+    <a href="config.php" class="btn btn-primary">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
       </svg>
