1fd0e1d5cceda2afe3c7aafa2703718fa9060c5b
Anson Mansfield <amansfield@mantaro.com>
2021-02-19 11:28:12 -0500

    yaml toggle button

--- deb/opt/gunshot/www/index.php
+++ deb/opt/gunshot/www/index.php
@@ -338,11 +338,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   </div>
   <div class="card">
     <div class="card-body">
-      <a href="password.php" class="btn btn-primary">
-        Password Changer
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+      <a href="<?=$_GET["yaml"] ? "yaml=0" : "?yaml=1"?>" class="btn <?=$_GET["yaml"] ? "btn-info" : "btn-info-outline"?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code-square" viewBox="0 0 16 16">
+          <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+          <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z"/>
         </svg>
+        View as YAML
       </a>
       <a href="password.php" class="btn btn-primary">
         Password Changer
