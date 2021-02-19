083964676c7be7d9952081c47c80b38571596d30
Anson Mansfield <amansfield@mantaro.com>
2021-02-19 12:01:29 -0500

    more consistent styling

--- deb/opt/gunshot/www/index.php
+++ deb/opt/gunshot/www/index.php
@@ -346,7 +346,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         View as YAML
       </a>
       <a href="password.php" class="btn btn-primary">
-        Password Changer
+        User Management
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
         </svg>
@@ -354,12 +354,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
   </div>
   <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <?= displaySettingForm($settings, $config, "config") ?>
-  <div class="card position-sticky" style="bottom: 0; z-index: 4;">
-    <div class="card-body">
-      <button type="submit" class="btn btn-primary">Apply Settings and Restart Affected Services</button>
+    <?= displaySettingForm($settings, $config, "config") ?>
+    <div class="card position-sticky" style="bottom: 0; z-index: 4;">
+      <div class="card-body">
+        <button type="submit" class="btn btn-primary">Apply Settings and Restart Affected Services</button>
+      </div>
     </div>
-  </div>
   </form>
 </div>
 
--- deb/opt/gunshot/www/password.php
+++ deb/opt/gunshot/www/password.php
@@ -42,97 +42,99 @@ ini_set('display_errors',1); error_reporting(E_ALL);
 
 
 <div class="container">
-<h1 class="title">User Management</h1>
-<div class="card">
-  <div class="card-body">
-    <a href="index.php" class="btn btn-primary">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-      </svg>
-      Main Configuration
-    </a>
+  <div class="pb-2 mt-4 mb-2 border-bottom">
+    <h1>User Management</h1>
   </div>
-</div>
-<div class="card">
-  <div class="card-header h4">Change Password</div>
-  <div class="card-body">
-    <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-      <div class="form-group row">
-        <label for="user" class="col-sm-3 col-form-label">Username:</label>
-        <div class="col-sm-9">
-          <input id="user" class="form-control"
-            name="user" type="text" autocomplete="username" required readonly
-            value="<?=htmlspecialchars($_SERVER['PHP_AUTH_USER'])?>"/>
-        </div>
-      </div>
-
-      <div class="form-group row">
-        <label for="curpass" class="col-sm-3 col-form-label">Current Password:</label>
-        <div class="col-sm-9">
-          <input id="curpass" class="form-control"
-            name="curpass" type="password" autocomplete="current-password" required />
-        </div>
-      </div>
-
-      <div class="form-group row">
-        <label for="newpass" class="col-sm-3 col-form-label">New Password:</label>
-        <div class="col-sm-9">
-          <input id="newpass" class="form-control"
-            name="newpass" type="password" autocomplete="new-password" required />
-        </div>
-      </div>
-
-      <div class="form-group row">
-        <label for="conpass" class="col-sm-3 col-form-label">Confirm Password:</label>
-        <div class="col-sm-9">
-          <input id="conpass" class="form-control"
-            name="conpass" type="password" autocomplete="new-password" required />
-        </div>
-      </div>
-      
-      <div>
-        <button type="submit" class="btn btn-primary">Change Password</button>
-      </div>
-    </form>
+  <div class="card">
+    <div class="card-body">
+      <a href="index.php" class="btn btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+        </svg>
+        Proxy Configuration
+      </a>
+    </div>
   </div>
+  <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <div class="card">
+      <div class="card-header h4">Change Password</div>
+      <div class="card-body">
+        <div class="form-group row">
+          <label for="user" class="col-sm-3 col-form-label">Username:</label>
+          <div class="col-sm-9">
+            <input id="user" class="form-control"
+              name="user" type="text" autocomplete="username" required readonly
+              value="<?=htmlspecialchars($_SERVER['PHP_AUTH_USER'])?>"/>
+          </div>
+        </div>
 
-  <?php
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $command = chpasswd($_POST["user"], $_POST["curpass"], $_POST["newpass"], $_POST["conpass"]);
-    exec($command, $output, $return);
+        <div class="form-group row">
+          <label for="curpass" class="col-sm-3 col-form-label">Current Password:</label>
+          <div class="col-sm-9">
+            <input id="curpass" class="form-control"
+              name="curpass" type="password" autocomplete="current-password" required />
+          </div>
+        </div>
 
-    if ($return == 0) {
-      ?><div class="card-footer alert-success" role="alert">
-        <p>Password successfully updated!</p>
-      </div><?php
-    } else {
-      ?><div class="card-footer alert-danger" role="alert">
-        <p>Password change not accepted!</p>
-        <ul>
-          <li>Ensure the current password you entered is correct.</li>
-          <li>Ensure you typed the new password correctly.</li>
-          <li>Ensure your new password meets the password length and complexity requirements for this system.</li>
-        </ul>
+        <div class="form-group row">
+          <label for="newpass" class="col-sm-3 col-form-label">New Password:</label>
+          <div class="col-sm-9">
+            <input id="newpass" class="form-control"
+              name="newpass" type="password" autocomplete="new-password" required />
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <label for="conpass" class="col-sm-3 col-form-label">Confirm Password:</label>
+          <div class="col-sm-9">
+            <input id="conpass" class="form-control"
+              name="conpass" type="password" autocomplete="new-password" required />
+          </div>
+        </div>
+        
+        <div>
+          <button type="submit" class="btn btn-primary">Change Password</button>
+        </div>
+      </div>
       <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          $command = chpasswd($_POST["user"], $_POST["curpass"], $_POST["newpass"], $_POST["conpass"]);
+          exec($command, $output, $return);
 
-      $pass_output = array();
-      foreach ($output as $line) {
-        if(preg_match("/passwd: .*$/", $line, $matches)){
-          array_push($pass_output, $matches[0]);
+          if ($return == 0) {
+            ?><div class="card-footer alert-success" role="alert">
+              <p>Password successfully updated!</p>
+            </div><?php
+          } else {
+            ?><div class="card-footer alert-danger" role="alert">
+              <p>Password change not accepted!</p>
+              <ul>
+                <li>Ensure the current password you entered is correct.</li>
+                <li>Ensure you typed the new password correctly.</li>
+                <li>Ensure your new password meets the password length and complexity requirements for this system.</li>
+              </ul>
+            <?php
+
+            $pass_output = array();
+            foreach ($output as $line) {
+              if(preg_match("/passwd: .*$/", $line, $matches)){
+                array_push($pass_output, $matches[0]);
+              }
+            }
+
+            ?><pre><code><?php
+              echo '$ passwd '.htmlspecialchars($_POST["user"])."\n";
+              foreach ($pass_output as $line) {
+                echo htmlspecialchars($line)."\n";
+              }
+            ?></code></pre><?php
+
+            ?></div><?php
+          }
         }
-      }
-
-      ?><pre><code><?php
-        echo '$ passwd '.htmlspecialchars($_POST["user"])."\n";
-        foreach ($pass_output as $line) {
-          echo htmlspecialchars($line)."\n";
-        }
-      ?></code></pre><?php
-
-      ?></div><?php
-    }
-  }
-  ?>
+      ?>
+    </div>
+  </form>
 </div>
 
 <!-- Optional JavaScript -->
